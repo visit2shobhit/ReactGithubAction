f8a3a37bc57bb8e7eca34aaa4786c96b0f60e797
visit2shobhit <shobhit833@gmail.com>
2026-02-21 10:58:27 -0800

    First Project

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>CI/CD using Github Actions</h1>
-      <h2>For beginners v2</h2>
-      <h2>With 2 projects</h2>
+      <h1>CI/CD Github Actions</h1>
     </>
   )
 }
